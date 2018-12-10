72b944badbf0e005ed7183f0161e6a4a9a8ed7e6
Jonathan Whittle <jonathan.m.whittle@gmail.com>
2018-12-10 12:08:52 -0500

    add authenticated user to nav bar logout

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,15 +1,18 @@
 <div class="">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand">Request Manager</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="/requestmanager">Home <span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link" href="/">TechApp</a>
-                    <a class="nav-item nav-link" href="/logout">Log out</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand">Request Manager</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="/requestmanager">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="/">TechApp</a>
+                {{--  Add logged in user to header  --}}
+                <div class="d-flex w-75 justify-content-end">
+                    <a class="nav-item nav-link"  href="/logout">Log out {{ Session::get('user') }}</a>
                 </div>
             </div>
-        </nav>
+        </div>
+    </nav>
 </div>
