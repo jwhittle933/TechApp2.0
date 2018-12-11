088a7e683716cfb4ffb71addd8860c614bb622df
Jonathan Whittle <jonathan.m.whittle@gmail.com>
2018-12-11 11:52:31 -0500

    remove api.php authorization restrictions

--- routes/api.php
+++ routes/api.php
@@ -5,18 +5,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::resource('/rooms', 'RoomsController',[
-    'except' => ['edit', 'show', 'store', 'create']
-]);
-Route::resource('/problems', 'ProblemsController', [
-    'except' => ['edit', 'show', 'store', 'create']
-]);
-Route::resource('/suggestions', 'SuggestionsController', [
-    'except' => ['edit', 'show', 'store', 'create']
-]);
-Route::resource('/solution', 'SolutionController',[
-    'except' => ['edit', 'show', 'store', 'create']
-]);
-Route::resource('/logout', 'LogOutController',[
-    'except' => ['edit', 'show', 'store', 'create']
-]);
+Route::resource('/rooms', 'RoomsController');
+Route::resource('/problems', 'ProblemsController');
+Route::resource('/suggestions', 'SuggestionsController');
+Route::resource('/solution', 'SolutionController');
+Route::resource('/logout', 'LogOutController');
